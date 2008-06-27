dc8deab1f8aa8cb194eac164339d6a4eeb61fe4f
Zach Beane <xach@xach.com>
2008-06-27 16:43:48 -0400

    - Update to make things actually work

--- gradient.lisp
+++ gradient.lisp
@@ -28,13 +28,6 @@
 
 (in-package #:vecto)
 
-(defun gradient-parameter (x y x0 y0 x1 y1)
-  (let ((numerator (+ (* (- x1 x0) (- x x0))
-                      (* (- y1 y0) (- y y0))))
-        (denominator (+ (expt (- x1 x0) 2)
-                        (expt (- y1 y0) 2))))
-    (/ numerator denominator)))
-
 (defun gradient-parameter-fun (x0 y0 x1 y1)
   (lambda (x y)
     (let ((numerator (+ (* (- x1 x0) (- x x0))
@@ -43,50 +36,44 @@
                           (expt (- y1 y0) 2))))
       (/ numerator denominator))))
 
-(defun sawtooth (param)
-  (cond ((< param 0)
-         (sawtooth (abs param)))
-        ((< 1 param)
-         (- 1 (nth-value 1 (truncate param))))
-        (t param)))
-
-(defun stitch-extend (param)
+(defun extend-domain (param)
   (clamp-range 0 param 1))
+
+(defun bilinear-domain (param)
+  (let ((param (* 2 (clamp-range 0 param 1))))
+    (if (<= param 1)
+        param
+        (- 2 param))))
 
 (defun set-gradient (x0 y0
                      r0 g0 b0 a0
                      x1 y1
                      r1 g1 b1 a1
-                     &key (stitch 'stitch-extend))
+                     &key
+                     (extend-start t)
+                     (extend-end t)
+                     (domain-function 'extend-domain))
   (let* ((matrix (transform-matrix *graphics-state*))
          (fun (make-transform-function (invert-matrix matrix)))
          (gfun (gradient-parameter-fun x0 y0 x1 y1)))
-    (lambda (x y)
-      (let ((param (float-octet (funcall stitch
-                                 (multiple-value-call gfun
-                                   (funcall fun x y))))))
-        (values (lerp r0 r1 param)
-                (lerp g0 g1 param)
-                (lerp b0 b1 param)
-                (lerp a0 a1 param))))))
-
-(defun star (short long points)
-  (move-to 0 long)
-  (let* ((increment (/ pi points))
-         (angle (- (/ pi 4) increment))
-         (lengths (vector short long)))
-    (dotimes (i (* points 2))
-      (let ((length (aref lengths (mod i 2))))
-        (line-to (* (cos angle) length)
-                 (* (sin angle) length)))
-      (incf angle increment))))
-
-(defun test-gradient (file)
-  (with-canvas (:width 900 :height 900)
-    (translate 400 400)
-    (centered-circle-path 0 100 10)
-    (stroke)
-    (star 50 100 5)
-    (set-rgba-fill 0 0 0 0.5)
-    (fill-path)
-    (save-png file)))
+    (setf r0 (float-octet r0)
+          g0 (float-octet g0)
+          b0 (float-octet b0)
+          a0 (float-octet a0)
+          r1 (float-octet r1)
+          g1 (float-octet g1)
+          b1 (float-octet b1)
+          a1 (float-octet a1))
+    (setf (fill-source *graphics-state*)
+          (lambda (x y)
+            (let ((param (multiple-value-call gfun (funcall fun x y))))
+              (cond ((and (< param 0) (not extend-start))
+                     (values 0 0 0 0))
+                    ((and (< 1 param) (not extend-end))
+                     (values 0 0 0 0))
+                    (t
+                     (setf param (float-octet (funcall domain-function param)))
+                     (values (lerp r0 r1 param)
+                             (lerp g0 g1 param)
+                             (lerp b0 b1 param)
+                             (lerp a0 a1 param)))))))))
